da7d6e9e21def804b78bb4f385d7d7c9c6a7f634
Luis12035 <57756350+Luis12035@users.noreply.github.com>
2021-12-07 00:07:45 -0600

    Lista en tabla

--- src/components/historicList/index.js
+++ src/components/historicList/index.js
@@ -36,30 +36,53 @@ const HistoricList = ()=>{
     return(
         <Page title="Historico" showHeader showNavBar>
       <Content>
-          
+        <div className="MainConteiner" style={{borderWidth: '1px', marginTop: '0.3em', padding: '0.4em', borderRadius: '14px', boxShadow: '2px 2px 8px #9E9E9E'}}>
+        <div className="MainListCointainer" style={{display: 'flex', marginTop: '0.3em'}}>
+          <div style={{borderStyle: 'solid', borderColor: 'black', borderWidth: '1px 1px 0.5px 0.5px', width: '100%'}}>
+            Codigo Clase
+          </div>
+          <div style={{borderStyle: 'solid', borderColor: 'black', borderWidth: '1px 1px 0.5px 0.5px', width: '100%'}}>
+            Clase
+          </div>
+          <div style={{borderStyle: 'solid', borderColor: 'black', borderWidth: '1px 1px 0.5px 0.5px', width: '100%'}}>
+            Año
+          </div>
+          <div style={{borderStyle: 'solid', borderColor: 'black', borderWidth: '1px 1px 0.5px 0.5px', width: '100%'}}>
+            Periodo
+          </div>
+          <div style={{borderStyle: 'solid', borderColor: 'black', borderWidth: '1px 1px 0.5px 0.5px', width: '100%'}}>
+            Nota
+          </div>
+        </div>  
                   {historic.items.map((i)=>{
-                      return (<ListItem 
-                        key = {i._id}
-                        
-                        > 
-                        <br/>{i.nombre}<br/>
+                      return (<ListItem key = {i._id}>
                         {i.historial.map((h)=>{
                             return (
-                            <ListItem key={h.Codigo}>
-                                {h.Codigo}<br/>
-                                {h.Clase}<br/>
-                                {h.Año}<br/>
-                                {h.Periodo}<br/>
-                                {h.Nota}                                
+                            <ListItem key={h.Codigo} style={{}}>
+                              <div className="MainListCointainer" style={{display: 'flex', borderWidth: '2px'}}>
+                                <div style={{borderStyle: 'solid', borderColor: 'black', borderWidth: '1px 0.5px 0.5px 1px', width: '100%'}}>
+                                  {h.Codigo}
+                                </div>
+                                <div style={{borderStyle: 'solid', borderColor: 'black', borderWidth: '1px 0.5px 0.5px 1px', width: '100%'}}>
+                                  {h.Clase}
+                                </div>
+                                <div style={{borderStyle: 'solid', borderColor: 'black', borderWidth: '1px 0.5px 0.5px 1px', width: '100%'}}>
+                                  {h.Año}
+                                </div>
+                                <div style={{borderStyle: 'solid', borderColor: 'black', borderWidth: '1px 0.5px 0.5px 1px', width: '100%'}}>
+                                  {h.Periodo}
+                                </div>
+                                <div style={{borderStyle: 'solid', borderColor: 'black', borderWidth: '1px 0.5px 0.5px 1px', width: '100%'}}>
+                                  {h.Nota} 
+                                </div>
+                              </div>                                                               
                              </ListItem>)
-                        })}    
-                        {i.user_id}<br/>
-                        
+                        })}                            
                         </ListItem>) 
                       
                       
                   })}
-       
+        </div>    
       </Content>
     </Page>
     );
